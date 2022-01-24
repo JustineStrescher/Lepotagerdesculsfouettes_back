d405f4a57fea28612dad9cda348282c93fb71e65
JustineStrescher <justine.cioe@gmail.com>
2022-01-24 10:15:53 +0100

    work in progress Productcontroller

--- src/Controller/Api/ProductController.php
+++ src/Controller/Api/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +10,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     /**
+     * Get product
      * @Route("/api/product", name="api_product")
      */
-    public function index(): Response
+    public function getProducts(ProductRepository $productRepository): Response
+    
     {
+        // @todo : retourner tous les produits de la BDD
+
+        // On va chercher les données
+        $productsList = $productRepository->findAll();
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Api/ProductController.php',
+             // Les données à sérialiser (à convertir en JSON)
+             $productsList,
+             // Le status code
+             200,
+             // Les en-têtes de réponse à ajouter (aucune)
+             [],
+             // Les groupes à utiliser par le Serializer
+             ['groups' => 'get_product']
         ]);
     }
 }
