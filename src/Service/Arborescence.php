a72b6100b7df2bf49bb349a5b66bc8e19f5b1d22
SimonDelaporte1 <simondelaporte@gmail.com>
2022-01-28 17:31:08 +0100

    correction data format in tree structure

--- src/Service/Arborescence.php
+++ src/Service/Arborescence.php
@@ -11,13 +11,30 @@ Class Arborescence {
         $this->categoryRepository = $categoryRepository;
     }
 
-    public function getArboCat($id, $output_array = array())
+    /**
+     * 
+     * Retourne toute l'arborescence ascendante d'une catégorie donnée
+     * 
+     */
+    public function getArboCat($id, $slugArray = array())
     {
+        // on donne un id de catégorie en paramètre, on récupère son parent
         $ParentCategory = $this->categoryRepository->findParentCategory($id);
-        $output_array[$ParentCategory['slug']] = $id;
+        // on stock le slug de la catégorie dans le tableau qui sera retourné
+        $slugArray[] = $ParentCategory['slug'];
         if ($ParentCategory['parent_id'] !== null) {
-            return $this->getArboCat($ParentCategory['parent_id'], $output_array);
+            // On appel la fonction de façon récursive jusqu'a ce que l'on arrive à la categorie mère
+            return $this->getArboCat($ParentCategory['parent_id'], $slugArray);
         } else {
+            // Le tableau est complété, on va le mettre dans le bon ordre, il faut que le tableau commence par la catégorie mère
+            $slugArray = array_reverse($slugArray);
+            $i=0;
+            foreach($slugArray as $thisSlug) {
+                // on va remplir le tableau de sortie avec des index spécifiques
+                // Dans la clé, on ajoute le mot Sub pour chaque niveau de catégorie, pour avoir une clé unique en dur exploitable par le front-office
+                $output_array[str_repeat('Sub', $i) . 'Category'] = $thisSlug;
+                $i++;
+            }
             return $output_array;
         }
     }
