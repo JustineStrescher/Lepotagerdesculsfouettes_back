6dbfc1acfe510318d35c47b6a1bd5f9aa83df029
SimonDelaporte1 <simondelaporte@gmail.com>
2022-02-02 17:03:56 +0100

    add postBasket controller

--- src/Controller/Api/CommandController.php
+++ src/Controller/Api/CommandController.php
@@ -3,11 +3,14 @@
 namespace App\Controller\Api;
 
 use App\Entity\Command;
+use App\Entity\ProductCommand;
+use App\Repository\ProductRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CommandController extends AbstractController
@@ -32,4 +35,137 @@ class CommandController extends AbstractController
             ]
         );
     }
+
+    /**
+     * 
+     * @Route("/api/client/basket", name="api_client_basket_post", methods={"POST"})
+     */
+    public function postBasket(Request $request, ManagerRegistry $doctrine, ValidatorInterface $validator, ProductRepository $productRepository)
+    {
+        // 20% de taxe sur les produits
+        $taxe_rate = 20;
+
+        //Récuperer le contenu JSON.
+        /*
+        "products": [
+            {
+              "id": 2,
+              "quantity": "2"
+            },
+            {
+              "id": 3,
+              "quantity": "1"
+            }
+          ]
+        */
+
+        $jsonContent=$request->getContent();
+
+        $productsArrayFromCart = json_decode($jsonContent);
+        // on va d'abord créer la commande dans la table command, pour cela il nous faut l'utilisateur connecté.
+        $user = $this->getUser();
+
+        // une fois en possession de l'id de la commande, on pourra remplir product_command
+        $commandArray = array(
+            'num_fact' => '#Fact_' . str_pad($user->getId() . count($user->getCommands()), 6, "0", STR_PAD_LEFT),
+            'status' => 'En cours',
+            'total_ttc' => 0,
+            'total_ht' => 0,
+            'total_tva' => 0,
+        );
+
+        foreach($productsArrayFromCart as $thisProduct){
+            // récupération des infos du produit
+            $product = $productRepository->find($thisProduct->id);
+            $productArray = array();
+            $productArray['unit_price'] = $product->getPrice();
+            $productArray['ttc'] = $product->getPrice() * $thisProduct->quantity;
+            $productArray['ht'] = $productArray['ttc'] * (1 - ($taxe_rate/100)); 
+            $productArray['tva'] = $productArray['ttc'] - $productArray['ht'];
+            $productArray['quantity'] = $thisProduct->quantity;
+            $productArray['entity'] = $product;
+            $productsArray[] = $productArray;
+
+            // MAJ du tableau général de commande
+            $commandArray['total_ttc'] += $productArray['ttc'];
+            $commandArray['total_ht'] += $productArray['ht'];
+            $commandArray['total_tva'] += $productArray['tva'];
+        }
+
+        // Création de l'entité que l'on complète avec les données calculées au dessus
+        $command = new Command();
+        $command->setUser($user);
+        $command->setStatus($commandArray['status']);
+        $command->setTotalHT($commandArray['total_ht']);
+        $command->setTotalTTC($commandArray['total_ttc']);
+        $command->setTotalTVA($commandArray['total_tva']);
+        $command->setNumFact($commandArray['num_fact']);
+
+        // Valider l'entité
+        // @link : https://symfony.com/doc/current/validation.html#using-the-validator-service
+        $errors = $validator->validate($command);
+        // Y'a-t-il des erreurs ?
+        if (count($errors) > 0) {
+            // tableau de retour
+            $errorsClean = [];
+            // @Retourner des erreurs de validation propres
+            /** @var ConstraintViolation $error */
+            foreach ($errors as $error) {
+                $errorsClean[$error->getPropertyPath()][] = $error->getMessage();
+            };
+
+            return $this->json($errorsClean, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // enregistrement des données dans la base
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($command);
+        $entityManager->flush();
+
+        // on a la commande en base de données, on va lui associer les produits du panier dont toutes les informations sont dans le tableau $productsArray
+        foreach($productsArray as $thisProductArray) {
+            $productCommand = new ProductCommand();
+            $productCommand->setTotalTTC($thisProductArray['ttc']);
+            $productCommand->setTotalTVA($thisProductArray['tva']);
+            $productCommand->setTotalHT($thisProductArray['ht']);
+            $productCommand->setQuantity($thisProductArray['quantity']);
+            $productCommand->setProduct($thisProductArray['entity']);
+            $productCommand->setUnitPrice($thisProductArray['unit_price']);
+            $productCommand->setCommand($command);
+
+            // Valider l'entité
+            // @link : https://symfony.com/doc/current/validation.html#using-the-validator-service
+            $errors = $validator->validate($productCommand);
+            // Y'a-t-il des erreurs ?
+            if (count($errors) > 0) {
+                // tableau de retour
+                $errorsClean = [];
+                // @Retourner des erreurs de validation propres
+                /** @var ConstraintViolation $error */
+                foreach ($errors as $error) {
+                    $errorsClean[$error->getPropertyPath()][] = $error->getMessage();
+                };
+
+                return $this->json($errorsClean, Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            // enregistrement des données dans la base
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($productCommand);
+        }
+        $entityManager->flush();
+        
+        // jusqu'ici tout va bien, on retourne un message de validation
+        $output = ['message'=>'OK'];
+
+        // On retourne la réponse adapté (201)
+        //dd($user);
+        return $this->json(
+            //Le client modifié peut etre ajouté en retour
+            $output,
+            //Le status code : 201 CREATED
+            //utilisation des constantes de classes
+            Response::HTTP_CREATED
+        );
+    }
+
 }
